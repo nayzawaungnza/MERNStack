26cd96e3a46cc7e57690a3803cced899283d2ef8
nayzawaungnza <nayzawaung750@gmail.com>
2024-07-03 11:25:44 +0630

    Ep-91-Mongo-atlas-db-setup

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,19 @@ const express = require("express");
 require("dotenv").config();
 const morgan = require("morgan");
 const recipesRoute = require("./routes/recipes");
+const mongoose = require("mongoose");
 
 const app = express();
+const mongoUrl =
+  "mongodb+srv://nayzawaung750:jkMYxLbkDDyVv5yd@cluster0.lwashty.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(mongoUrl).then(() => {
+  console.log("Connected to MongoDB");
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}`);
+  });
+});
 
 app.use(morgan("dev"));
 app.use("/api/recipes", recipesRoute);
@@ -12,7 +23,3 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port http://localhost:${PORT}`);
-});
